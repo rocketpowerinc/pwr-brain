6b76f294694bf6fbaddcce0ae4a26301f71c74e0
ReevesA1 <adamrivard87@gmail.com>
2026-03-04 20:41:04 -0500

    s

--- fresh-start.ps1
+++ fresh-start.ps1
@@ -1,5 +1,14 @@
-Remove-Item "C:\Users\rocket\pwr-brain" -Recurse -Force
-Set-location $env:USERPROFILE
-git clone https://github.com/rocketpowerinc/pwr-brain.git
-set-location .\pwr-brain\
+# Set up temp path for cloning
+$tempPath = Join-Path $env:TEMP "pwr-brain-tmp"
+if (Test-Path $tempPath) { Remove-Item $tempPath -Recurse -Force }
+
+# Clone to temp
+git clone https://github.com/rocketpowerinc/pwr-brain.git $tempPath
+
+# Run the initializer from the temp repo
+Set-Location $tempPath
 pwsh scripts/init-pwr-brain.ps1
+
+# Go back and clean up
+Set-Location $env:USERPROFILE
+Remove-Item $tempPath -Recurse -Force
